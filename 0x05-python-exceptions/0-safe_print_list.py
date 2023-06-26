f892fa87bd0754724e3a700c1bc9c63d6a466c1b
Anotibills <anotibills@gmail.com>
2023-06-26 15:16:09 -0700

    Correcting errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     num = 0
-    for a in range(x):
+    for j in range(x):
         try:
-            print(my_list[a], end="")
+            print(my_list[j], end="")
             num += 1
         except IndexError:
             break
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
      count = 0
-    for a in range(x):
+    for j in range(x):
         try:
-            print("{:d}".format(my_list[a]), end="")
+            print("{:d}".format(my_list[j]), end="")
             count += 1
         except (TypeError, ValueError):
             pass
