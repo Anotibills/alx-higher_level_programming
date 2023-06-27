c7ab22eda0256ead5ff9b9cd06c67b826ea46d2b
Anotibills <anotibills@gmail.com>
2023-06-26 21:02:13 -0700

    Advance safe print integer with error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ import sys
 def safe_print_integer_err(value):
      try:
         print("{:d}".format(value))
-    except (TypeError, ValueError) as err:
+    except (ValueError, TypeError) as err:
         print("Exception: {}".format(err), file=sys.stderr)
         return False
     else:
