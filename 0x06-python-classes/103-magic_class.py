70e3508ef2bed16eebc97704d8e487b607401bac
Anotibills <anotibills@gmail.com>
2023-06-27 14:52:35 -0700

    1103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,11 +3,13 @@
 
 import math
 
+
 class MagicClass:
     """Represent a circle."""
 
     def __init__(self, radius=0):
         """Initialize a MagicClass.
+
         Arg:
             radius (float or int): The radius of the new MagicClass.
         """
