5b71d24a9e765a1d655940c448dbf4769c9c2c51
Anotibills <anotibills@gmail.com>
2023-06-26 22:06:02 -0700

    Advance 4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     ret = 0
-    for j in range(1, 3):
+    for i in range(1, 3):
         try:
-            if j > a:
-                raise Exception ("Too Far")
+            if i > a:
+                raise Exception('Too far')
             else:
-                ret += a ** b / j
+                ret += a ** b / i
         except Exception:
-            result = b + a
+            ret = b + a
             break
-        return (ret)
+    return (ret)
