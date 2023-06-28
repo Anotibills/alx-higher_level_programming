df92971308933a110eef9dbe0af8655f81aff48f
Anotibills <anotibills@gmail.com>
2023-06-27 21:15:15 -0700

    Classes 102

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Define a MagicClass."""
-
 import math
 
 class MagicClass:
@@ -10,7 +9,7 @@ class MagicClass:
         """Initialize a MagicClass.
 
         Arg:
-            radius (float or int): The radius of the new MagicClass.
+            radius (float or int): Radius of the new MagicClass.
         """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
-
+"""Define a class Square."""
 class Square:
-    """This represent a square."""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """This initializes a new square.
+        """Initialize a new square.
         Args:
-            size (int): This is the size of the new square.
+            size (int): The size of the new square.
         """
-        self.size = size
-
-    @property
-    def size(self):
-        """This get/set the current size of a square."""
-        return (self.__size)
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
+        if not isinstance(int, size):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        self.__size = size
 
     def area(self):
-        """This returns the current area of the square."""
+        """Return the current area of the square."""
         return (self.__size * self.__size)
