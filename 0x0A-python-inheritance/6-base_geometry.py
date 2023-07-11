9dcd3302569b25c7019fbe0180ac2d46589e8f1d
anoti <anotibills@gmail.com>
2023-07-11 03:27:43 +0100

    python implementation tasks solved

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+class BaseGeometry:
+    """Represent the base geometry."""
+
+    def area(self):
+        """Not yet implemented."""
+        raise Exception("area() is not implemented")
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+This is tghe project on Inheritance
