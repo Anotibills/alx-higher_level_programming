ff7d23d05555040206708707cdc9dfc0bf9d16a6
Tolulope Abiola <anotibills@gmail.com>
2023-08-17 22:04:30 +0100

    hbtn file imported 1

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,4 +1,4 @@
--- a script that lists all genres in the database hbtn_0d_tvshows_rate 
+--The script that lists all genres in database hbtn_0d_tvshows_rate 
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
