6341189217137dc4fd991f04b287e96d5a5d33b0
Tolulope Abiola <anotibills@gmail.com>
2023-09-12 23:02:54 +0100

    correction

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,15 +7,14 @@ class Rectangle {
     }
   }
 
-  print() {
-    for (let a = 0; a < this.height; a++) {
-      let row = '';
-      for (let b = 0; b < this.width; b++) {
-        row += 'X';
-      }
-      console.log(row);
-    }
-  }
+print() {
+	for (let a = 0; a < this.height; a++) {
+		let row = '';
+		for (let b = 0; b < this.width; b++) {
+			row += 'X';
+		}
+		console.log(row);
+	}
 }
-
+}
 module.exports = Rectangle;
