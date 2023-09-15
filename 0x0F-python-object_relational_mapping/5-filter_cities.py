ace513febdda1e680293beda07b8e51ea6b16d16
Tolulope Abiola <anotibills@gmail.com>
2023-09-15 21:00:34 +0100

    Task 5 corrected

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,57 +1,42 @@
 #!/usr/bin/python3
-"""Script that takes in the name of a state as an argument
-and lists all cities from the database hbtn_0e_0_usa"""
+"""script that lists all cities in a specific state from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    '''Check if the correct number of command-line arguments is provided'''
+if __name__ == "__main__":
+    '''
+    Check if the correct number of command-line arguments is provided
+    '''
     if len(sys.argv) != 5:
         print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
         sys.exit(1)
 
-    '''Get command-line arguments'''
+    # Get command-line arguments
     username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
+    '''
+    Create a database connection
+    '''
+    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+    cur = db.cursor()
+
     try:
-        '''Connect to the database'''
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database,
-            charset="utf8"
-        )
+        '''
+        Query and print the names of cities in the specified state
+        '''
+        cur.execute("""SELECT cities.name FROM
+                    cities INNER JOIN states ON states.id=cities.state_id
+                    WHERE states.name=%s""", (state_name,))
+        rows = cur.fetchall()
+        city_names = [row[0] for row in rows]
+        print(", ".join(city_names))
 
-        '''Create a cursor object'''
-        cur = db.cursor()
-
-        '''Execute the query to grab cities from the specified state'''
-        cur.execute("SELECT cities.name FROM cities "
-                    "LEFT OUTER JOIN states ON cities.state_id = states.id "
-                    "WHERE states.name = %s;", (state_name,))
-
-        '''Fetch all the rows'''
-        query_rows = cur.fetchall()
-
-        '''Create a list to store city names'''
-        city_names = [row[0] for row in query_rows]
-
-        '''Print the results'''
-        if city_names:
-            print(', '.join(city_names))
-        else:
-            print("No cities found for state: {}".format(state_name))
-
-    except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
+    except Exception as e:
+        print("Error: {}".format(e))
         sys.exit(1)
 
     finally:
-        '''Close the cursor and the database connection'''
-        if cur:
-            cur.close()
-        if db:
-            db.close()
+        # Close the cursor and database connection
+        cur.close()
+        db.close()
