1cc021327c27c1ce04e66786005ff20ef2c4f837
Tolulope Abiola <anotibills@gmail.com>
2023-09-28 18:56:36 +0100

    2 ts

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# script that sends a DELETE request to the URL passed as the first argument 
-[ -z "$1" ] && { echo "Usage: $0 <URL>"; exit 1; } || curl -X DELETE -s "$1"
+# script that sends a DELETE request to the URL passed as the first argument
+[ -z "$1" ] && { echo "Usage: $0 <URL>"; exit 1; } || { echo "I'm a DELETE request"; curl -X DELETE -s "$1"; }
 
