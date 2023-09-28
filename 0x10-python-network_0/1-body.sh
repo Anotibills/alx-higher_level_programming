e744510d94ba52ad62cb692dc7f08250c7db751d
Tolulope Abiola <anotibills@gmail.com>
2023-09-28 19:54:03 +0100

    Task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # The script that sends a GET request to a URL and displays the body of a 200 status code response.
-curl -sL $1
+curl -s -w "%{http_code}" "$1" | awk 'END{if($1==200)system("tail -n +2")}'
+
 
 
