f28c997ba84bc2adaac63967622bb98e946cfbd6
Tolulope Abiola <anotibills@gmail.com>
2023-09-28 18:46:05 +0100

    1 file again1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # The script that sends a GET request to a URL and displays the body of a 200 status code response.
-[ "$(curl -sI "$1" | grep -i '^HTTP' | grep -q ' 200')" ] && curl -s "$1"
+curl -s -L $1
 
 
