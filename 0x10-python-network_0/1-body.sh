72a9c41fefdc94854f3c3710b2feaed3f848899f
Tolulope Abiola <anotibills@gmail.com>
2023-09-28 18:36:39 +0100

    1 file

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script sends a GET request to a URL and displays the body of a 200 status code response at once.
-[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ] && curl -s "$1" || echo "Non-200 status code"
+[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ] && curl -s "$1"
 
