fa5d6bc8c0f1d95c1c87001da97761b18221a54b
Tolulope Abiola <anotibills@gmail.com>
2023-09-28 19:43:18 +0100

    Completed again 1

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# This script makes a request to 0.0.0.0:5000/catch_me and captures the response.
-curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
+# The script that makes a request to 0.0.0.0:5000/catch_me and captures the response.
+curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool" | grep -o "You got me!"
+
 
