01a47b600da7f4f623ca075387b9680cf1aee926
Tolulope Abiola <anotibills@gmail.com>
2023-09-29 21:02:33 +0100

    Task 0 corrected

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,21 +7,22 @@ import urllib.request
 url = "https://alx-intranet.hbtn.io/status"
 
 try:
-    '''Use 'urllib.request.urlopen' to make an HTTP GET request to the URL'''
+    '''Use 'urllib' to make an HTTP GET request to the URL'''
     with urllib.request.urlopen(url) as response:
         html = response.read()
 
         '''Print the response information with tabulation'''
         print("Body response:")
         print("\t- type:", type(html))
+        print("\t- content:", html)
 
         try:
             '''Attempt to decode the content using UTF-8'''
             content = html.decode('utf-8')
-            print("\t- content:", content)
+            print("\t- utf8 content:", content)
         except UnicodeDecodeError:
-            print("\t- content: Unable to decode as UTF-8")
+            print("\t- utf8 content: Unable to decode as UTF-8")
 
 except Exception as e:
-    '''Handle exceptions, if any'''
+    '''Handle exceptions'''
     print(e)
