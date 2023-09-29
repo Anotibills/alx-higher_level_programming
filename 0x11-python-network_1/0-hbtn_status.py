a6fca4f0a8e80c382d25e6b1e825573e2afdadef
Tolulope Abiola <anotibills@gmail.com>
2023-09-29 23:58:26 +0100

    This is 0 and 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """
-Script that fetches url
+This script fetches the specified URL and displays the response.
 """
 import urllib.request
 
-'''Define the URL to fetch'''
-url = "https://alx-intranet.hbtn.io/status"
 
-try:
-    '''Use 'urllib' to make an HTTP GET request to the URL'''
-    with urllib.request.urlopen(url) as response:
-        '''Read the response body as bytes'''
-        html = response.read()
+def fetch_and_display(url):
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
 
-        '''This prints the response information with tabulation'''
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(repr(html)))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+            '''This print the response body'''
+            print("Body response:")
+            print("\t- type: {}".format(type(html)))
+            print("\t- content: {}".format(html))
+            print("\t- utf8 content: {}".format(html.decode('utf-8')))
 
-except Exception as e:
-    ''' This handle exceptions'''
-    print("Error: {}".format(e))
+    except urllib.error.URLError as e:
+        print("Error:", e)
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    fetch_and_display(url)
